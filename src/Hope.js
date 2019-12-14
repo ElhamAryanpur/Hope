c2d33435df8594f8ec7a8d87c44c7bfbccb145fc
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-12-14 12:20:55 +0430

    doing tests

--- src/Hope.js
+++ src/Hope.js
@@ -1,14 +1,3 @@
-function loadDep(url="", callback=()=>{}) {
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-       callback(this.responseText);
-      }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
-
 class Hope{
     constructor(settings={}){
         this.settings = settings;
@@ -17,22 +6,29 @@ class Hope{
         this.elements = {};
 
         const deps = [
-            "https://code.jquery.com/jquery-3.4.1.min.js"
-        ]; for (var i=0; i<deps.length; i++){loadDep(deps[i]); (data)=>{
+            "https://raw.githubusercontent.com/tantaman/LargeLocalStorage/master/dist/LargeLocalStorage.min.js"
+        ]; for (var i=0; i<deps.length; i++){this.loadDep(deps[i]); (data)=>{
             eval(data);}}
         
-        loadDep("https://raw.githubusercontent.com/ElhamAryanpur/Hope/master/src/Hope.css", (response)=>{
+        this.loadDep("https://raw.githubusercontent.com/ElhamAryanpur/Hope/master/src/Hope.css",
+        (response)=>{
             const style = document.createElement("style");
             style.innerHTML = response;
             document.head.appendChild(style);
         });
-        
+
+        const cap = 50 * 1024 * 1024;
+        var storage = new LargeLocalStorage({
+            size: cap,
+            name: "test"
+        });
+
     }
 
-    run(){
-        if (this.settings.table != undefined){
+    init(settings={}){
+        if (settings.table != undefined){
             var tableDiv = document.createElement("div");
-            var table = this.settings.table.data;
+            var table = settings.table.data;
             this.elements.table = [];
 
             for (var i=0; i<table.length; i++){
@@ -55,10 +51,12 @@ class Hope{
                 for (var i=0;i<this.elements.table.length;i++){
                     data[this.elements.table[i].id] = this.elements.table[i].value;
                 } callback(data);
+
+                
             }
 
             submitButton.addEventListener("click", ()=>{
-                buttonCallBack(this.settings.table.callback)
+                buttonCallBack(settings.table.callback)
             })
 
             tableDiv.appendChild(document.createElement("br"));
@@ -66,4 +64,17 @@ class Hope{
             this.canvas.appendChild(tableDiv);
         }
     }
+/************************************************************************************************/
+    loadDep(url="", callback=()=>{}) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+        callback(this.responseText);
+        }
+        };
+        xhttp.open("GET", url, true);
+        xhttp.send();
+    }
+
+    
 }
