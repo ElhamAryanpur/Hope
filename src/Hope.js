16511d9901ab31f9a2a46bad7cb1d07a7a91f30f
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-12-14 19:14:22 +0430

    test

--- src/Hope.js
+++ src/Hope.js
@@ -77,7 +77,6 @@ global.Hope = class{
 
                 tableDB.data = data;
                 tableDB.saveData();
-                /** TODO - Compress And Save To Local Storage */
 
             }
 
@@ -94,9 +93,9 @@ global.Hope = class{
     _loadDep(url="", callback=()=>{}) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-        callback(this.responseText);
-        }
+            if (this.readyState == 4 && this.status == 200) {
+                callback(this.responseText);
+            }
         };
         xhttp.open("GET", url, true);
         xhttp.send();
