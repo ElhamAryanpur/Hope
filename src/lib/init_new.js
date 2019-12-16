347057701cad566447e71f4961995ef7b06e787e
Elham Aryanpur <elhamaryanpur5@gmail.com>
2019-12-16 12:37:52 +0430

    starting to do tests on new design

--- src/lib/init_new.js
+++ src/lib/init_new.js
@@ -1,5 +1,38 @@
-function init(settings={}){
+const DB = require("./db");
 
+function init(settings={}){
+    if (settings.table != undefined){
+        var initReturn = initOfInit(settings);
+        const tableSettings = initReturn[0]; const DIV = initReturn[1];
+        const db = new DB("table");
+        const table = document.createElement("table");
+        table.setAttribute("class", "hope-table");
+
+        makeRow({div: DIV});
+
+    }
+}
+
+/*==============================================================================*/
+
+function initOfInit(settings){
+    const tableSettings = settings.table;
+    const DIV = document.getElementById(tableSettings.name);
+    DIV.setAttribute("class", "hope-container");
+
+    const db = new DB("table-settings");
+    db.data = tableSettings.data;
+    db.saveData();
+
+    return [tableSettings, DIV];
+}
+
+function makeRow(settings){
+for (var i=0; i<5; i++){
+
+    
+
+}
 }
 
 module.exports = init;
