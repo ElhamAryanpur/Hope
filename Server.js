a54f3e88a4776d495501a5bcb18a4f3fa99e4c80
Elham Aryanpur <elhamaryanpur5@gmail.com>
2020-03-13 18:27:01 +0430

    made db ready to use

--- Server.js
+++ Server.js
@@ -1,7 +1,7 @@
 const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-const m = require("./lib/server/main");
+const m = require("./lib/main");
 const Main = new m();
 
 
