deec95936e85a924716707da3e83215dee9b875c
Elham Aryanpur <elhamaryanpur5@gmail.com>
2020-04-27 17:25:39 +0300

    test fix 9

--- src/table/TableNew.svelte
+++ src/table/TableNew.svelte
@@ -1,95 +1,115 @@
 <script>
-let numberOfFields = 2;
+  let numberOfFields = 2;
 
-$: fieldList = ((NOF)=>{
+  $: fieldList = (NOF => {
     const l = [];
-    for(var i=0; i<NOF; i++){
-        l.push("text");
+    for (var i = 0; i < NOF; i++) {
+      l.push("text");
     }
     return l;
-})(numberOfFields);
+  })(numberOfFields);
 
-function createTable(){
+  function createTable() {
     const data = {
-        name: tableName,
-        values: fieldValues,
-        types: fieldTypes
+      name: tableName,
+      values: fieldValues,
+      types: fieldTypes
     };
-    window.TableDB.get("tableNames", (doc)=>{
-        if (doc.names.includes(tableName) != true){
-            if (doc == null){
-            window.TableDB.put("tableNames", {names: [data.name]});
+    window.TableDB.get("tableNames", doc => {
+      if (Object.entries(doc).length != 0) {
+        if (doc.names.includes(tableName) != true) {
+          if (doc == null) {
+            window.TableDB.put("tableNames", { names: [data.name] });
             window.TableDB.put(data.name, data);
-        } else {
+          } else {
             const d = doc;
-            d.names.push(data.name)
+            d.names.push(data.name);
             window.TableDB.put("tableNames", d);
             window.TableDB.put(data.name, data);
+          }
+        } else {
+          window.TableDB.put("tableNames", { names: [data.name] });
+          window.TableDB.put(data.name, data);
         }
-        fieldValues = ['id', ...fieldValues]
-        fieldTypes = ['INTEGER PRIMARY KEY AUTOINCREMENT', ...fieldTypes]
+        fieldValues = ["id", ...fieldValues];
+        fieldTypes = ["INTEGER PRIMARY KEY AUTOINCREMENT", ...fieldTypes];
         const newData = {
-            name: tableName,
-            values: fieldValues,
-            types: fieldTypes
-        }
-        
-        window.socket.emit('new table', newData);
-        }
-        
-    })
+          name: tableName,
+          values: fieldValues,
+          types: fieldTypes
+        };
+
+        window.socket.emit("new table", newData);
+      }
+    });
 
     window.dialog_close(`new-table`);
-}
+  }
 
-let fieldValues = [];
-let fieldTypes = [];
-let tableName = "";
+  let fieldValues = [];
+  let fieldTypes = [];
+  let tableName = "";
 </script>
 
 <style>
-    tr td {
-        text-align: center;
-    }
+  tr td {
+    text-align: center;
+  }
 
-    button {
-        border: 1px solid #6AAAC9;
-        border-radius: 0px;
-    }
+  button {
+    border: 1px solid #6aaac9;
+    border-radius: 0px;
+  }
 
-    .style{
-        border: 1px solid #6AAAC9;
-        padding: 10px;
-        padding-top: 20px;
-        font-weight: bold;
-    }
+  .style {
+    border: 1px solid #6aaac9;
+    padding: 10px;
+    padding-top: 20px;
+    font-weight: bold;
+  }
 </style>
 
 <table>
-    <tr>
-        <td><span>Number Of Fields:</span></td>
-        <td><input type="number" bind:value={numberOfFields}></td>
-    </tr>
-    <br>
+  <tr>
+    <td>
+      <span>Number Of Fields:</span>
+    </td>
+    <td>
+      <input type="number" bind:value={numberOfFields} />
+    </td>
+  </tr>
+  <br />
+  <tr class="style">
+    <td class="style">Field Name</td>
+    <td class="style">Field Type</td>
+  </tr>
+  {#each fieldList as i, n}
     <tr class="style">
-        <td class="style">Field Name</td>
-        <td class="style">Field Type</td>
-    </tr>
-    {#each fieldList as i, n}
-        <tr class="style">
-            <td class="style"><input bind:value={fieldValues[`${n}`]} type="text" placeholder="Field {n + 1}"></td>
+      <td class="style">
+        <input
+          bind:value={fieldValues[`${n}`]}
+          type="text"
+          placeholder="Field {n + 1}" />
+      </td>
 
-            <td class="style">
-                <select bind:value={fieldTypes[`${n}`]}>
-                    <option value="text">text</option>
-                    <option value="number">number</option>
-                </select> <br>
-            </td>
-        </tr>
-    {/each}
-    <br> <br> <br>
-    <tr>
-        <td> <input bind:value={tableName} type="text" placeholder="Table Name"></td>
-        <td><button on:click={()=> createTable()}>Create Table</button></td>
+      <td class="style">
+        <select bind:value={fieldTypes[`${n}`]}>
+          <option value="text">text</option>
+          <option value="number">number</option>
+        </select>
+        <br />
+      </td>
     </tr>
+  {/each}
+  <br />
+  <br />
+  <br />
+  <tr>
+    <td>
+      <input bind:value={tableName} type="text" placeholder="Table Name" />
+    </td>
+    <td>
+      <button on:click={() => createTable()}>Create Table</button>
+    </td>
+  </tr>
 </table>
